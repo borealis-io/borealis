1c5403e98cc8b87ab82c61a67321aff10f21aeb1
Kevin Swiber <kswiber@gmail.com>
2013-12-03 13:54:18 -0500

    Fixing stop_app.sh.

--- stop_app.sh
+++ stop_app.sh
@@ -1,10 +1,15 @@
 set -e
 
+APP_PREFIX=app-
 HOSTNAME=$1
-HOST_IP_ADDR=$(docker inspect "$HOSTNAME" | json -a NetworkSettings.IPAddress)
+
+APP_NAME=$APP_PREFIX$HOSTNAME
+
+HOST_IP_ADDR=$(docker inspect "$APP_NAME" | json -a NetworkSettings.IPAddress)
 
 curl -i -X DELETE http://localhost:8081/apps/$HOSTNAME
 
-docker ps | grep "$HOSTNAME" | awk '{ print $1 }' | xargs docker stop
-docker ps -a | grep "$HOSTNAME" | awk '{ print $1 }' | xargs docker rm
+docker stop $APP_NAME
+docker rm $APP_NAME
+
 sudo iptables -S | grep $HOST_IP_ADDR.*docker0.*ACCEPT | awk '{ gsub("-A", "sudo iptables -D", $0); print $0 }' | xargs -0 /bin/bash -c
