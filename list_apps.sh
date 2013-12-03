a30aa213a21cad3e557d86a35e1056e0795a1992
Kevin Swiber <kswiber@gmail.com>
2013-12-03 13:45:28 -0500

    Putting app prefix in a variable for list_apps.

--- list_apps.sh
+++ list_apps.sh
@@ -1,15 +1,17 @@
+APP_PREFIX=app-
+
 if [ "$1" == "-q" ]
 then
 	docker ps -q | \
 		xargs docker inspect | \
 		json -a Name | \
 		awk '{ print substr($0, 2); }' | \
-		grep --color=never "^app-"
+		grep --color=never "^$APP_PREFIX"
 else
 	(echo NAME ID && docker ps -q | \
 		xargs docker inspect | \
 		json -a Name ID | \
 		awk '{ print substr($0, 2); }' | \
-		grep --color=never "^app-") | \
+		grep --color=never "^$APP_PREFIX") | \
 		column -t
 fi
