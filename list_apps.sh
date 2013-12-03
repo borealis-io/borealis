2a1f52d6fd916d0627634ba82b07dc5b07b06aaa
Kevin Swiber <kswiber@gmail.com>
2013-12-03 14:18:10 -0500

    Removing app- prefix from list_apps.

--- list_apps.sh
+++ list_apps.sh
@@ -1,4 +1,5 @@
 APP_PREFIX=app-
+APP_PREFIX_LENGTH=$((${#APP_PREFIX}+1)) # Adding one for the slash (e.g., /app-appname.hostname.io)
 
 if [ "$1" == "-q" ]
 then
@@ -6,12 +7,14 @@ then
 		xargs docker inspect | \
 		json -a Name | \
 		awk '{ print substr($0, 2); }' | \
-		grep --color=never "^$APP_PREFIX"
+		grep --color=never "^$APP_PREFIX" | \
+                awk "{ print substr(\$0, $APP_PREFIX_LENGTH); }"
 else
 	(echo NAME ID && docker ps -q | \
 		xargs docker inspect | \
 		json -a Name ID | \
 		awk '{ print substr($0, 2); }' | \
-		grep --color=never "^$APP_PREFIX") | \
+		grep --color=never "^$APP_PREFIX" | \
+                awk "{ print substr(\$0, $APP_PREFIX_LENGTH); }") | \
 		column -t
 fi
