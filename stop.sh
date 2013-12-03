0bd47e5cc65f1ce92522388354e93beae4c71598
Kevin Swiber <kswiber@gmail.com>
2013-12-03 14:37:56 -0500

    Removing stop.sh

--- stop.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-docker ps | grep "/app/bin/node" | awk '{ print $1 }' | xargs docker stop
-docker ps -a | grep "/app/bin/node" | awk '{ print $1 }' | xargs docker rm
-sudo iptables -S | grep docker0.*ACCEPT | awk '{ gsub("-A", "sudo iptables -D", $0); print $0 }' | xargs -0 /bin/bash -c
