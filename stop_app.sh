82476007de0ca7aafc3a2b9eae902c9868d4ec8f
Kevin Swiber <kswiber@gmail.com>
2013-12-03 14:34:48 -0500

    Making start_app and stop_app take multiple hostnames.

--- stop_app.sh
+++ stop_app.sh
@@ -1,15 +1,17 @@
 set -e
 
 APP_PREFIX=app-
-HOSTNAME=$1
 
-APP_NAME=$APP_PREFIX$HOSTNAME
-
-HOST_IP_ADDR=$(docker inspect "$APP_NAME" | json -a NetworkSettings.IPAddress)
-
-curl -i -X DELETE http://localhost:8081/apps/$HOSTNAME
-
-docker stop $APP_NAME
-docker rm $APP_NAME
-
-sudo iptables -S | grep $HOST_IP_ADDR.*docker0.*ACCEPT | awk '{ gsub("-A", "sudo iptables -D", $0); print $0 }' | xargs -0 /bin/bash -c
+for HOSTNAME in "$@"
+do
+	APP_NAME=$APP_PREFIX$HOSTNAME
+	
+	HOST_IP_ADDR=$(docker inspect "$APP_NAME" | json -a NetworkSettings.IPAddress)
+	
+	curl -i -X DELETE http://localhost:8081/apps/$HOSTNAME
+	
+	docker stop $APP_NAME
+	docker rm $APP_NAME
+	
+	sudo iptables -S | grep $HOST_IP_ADDR.*docker0.*ACCEPT | awk '{ gsub("-A", "sudo iptables -D", $0); print $0 }' | xargs -0 /bin/bash -c
+done
