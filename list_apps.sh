1b1e035424d85fde25f97dd72a56d8e05b0e331e
Kevin Swiber <kswiber@gmail.com>
2013-12-03 13:44:12 -0500

    Fixing format of list_apps

--- list_apps.sh
+++ list_apps.sh
@@ -6,11 +6,10 @@ then
 		awk '{ print substr($0, 2); }' | \
 		grep --color=never "^app-"
 else
-	docker ps -q | \
+	(echo NAME ID && docker ps -q | \
 		xargs docker inspect | \
 		json -a Name ID | \
 		awk '{ print substr($0, 2); }' | \
-		grep --color=never "^app-" | \
-		awk '{ print "NAME ID"; print $0; }' | \
+		grep --color=never "^app-") | \
 		column -t
 fi
