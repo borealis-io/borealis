48f298155441895dd86be1f3d567d20ee442c4e3
Kevin Swiber <kswiber@gmail.com>
2013-12-04 01:40:50 -0500

    Adding readable output on start_app and stop_app.

--- start_app.sh
+++ start_app.sh
@@ -2,13 +2,19 @@ set -e
 
 for HOSTNAME in "$@"
 do
+	echo "---> Starting app: $HOSTNAME"
+
 	APP_NAME=app-$HOSTNAME
 	PORT=5000
 	
 	ROUTER_IP_ADDR=$(docker inspect router | json -a NetworkSettings.IPAddress)
+
+	echo "---> Using router: $ROUTER_IP_ADDR"
 	
 	APP_CONTAINER_ID=$(docker run -t -d -p $PORT -e "PORT=$PORT" -e "SERVER_ID=$SERVER_ID" -name=$APP_NAME node-sample)
 	APP_IP_ADDR=$(docker inspect $APP_CONTAINER_ID | json -a NetworkSettings.IPAddress)
+
+	echo "---> App container up and running."
 	
 	sudo iptables -D FORWARD -i docker0 -o docker0 -j DROP
 	
@@ -16,6 +22,17 @@ do
 	sudo iptables -A FORWARD -s $APP_IP_ADDR/32 -d $ROUTER_IP_ADDR/32 -i docker0 -o docker0 -p tcp -m tcp --sport $PORT -j ACCEPT
 	
 	sudo iptables -A FORWARD -i docker0 -o docker0 -j DROP
+
+	echo "---> Network access set for routing."
 	 
-	curl -i -d "host=$HOSTNAME&target=http%3A%2F%2F$APP_IP_ADDR%3A$PORT" -X POST http://localhost:8081/apps
+	CODE=$(curl -s -o /dev/null -w "%{http_code}" -d "host=$HOSTNAME&target=http%3A%2F%2F$APP_IP_ADDR%3A$PORT" -X POST http://localhost:8081/apps)
+
+	if [ $CODE == "201" ]
+	then
+		echo "---> Host registered with router."
+	else
+		echo "---> Host NOT registered with router."
+	fi
+
+	echo "---> Done."
 done
--- stop_app.sh
+++ stop_app.sh
@@ -4,14 +4,28 @@ APP_PREFIX=app-
 
 for HOSTNAME in "$@"
 do
+	echo "---> Stopping app: $HOSTNAME"
+
 	APP_NAME=$APP_PREFIX$HOSTNAME
 	
 	HOST_IP_ADDR=$(docker inspect "$APP_NAME" | json -a NetworkSettings.IPAddress)
 	
-	curl -i -X DELETE http://localhost:8081/apps/$HOSTNAME
+	CODE=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE http://localhost:8081/apps/$HOSTNAME)
 	
-	docker stop $APP_NAME
-	docker rm $APP_NAME
+	if [ $CODE == "204" ]
+	then
+		echo "---> Host removed from router."
+	else
+		echo "---> Host not found in router."
+	fi
+
+	docker stop $APP_NAME > /dev/null
+	docker rm $APP_NAME > /dev/null
+
+	echo "---> App container removed."
 	
 	sudo iptables -S | grep $HOST_IP_ADDR.*docker0.*ACCEPT | awk '{ gsub("-A", "sudo iptables -D", $0); print $0 }' | xargs -0 /bin/bash -c
+	echo "---> Network access cleaned."
+
+	echo "---> Done."
 done
