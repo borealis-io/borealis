c8d739e4275befc323da5476e70c18d28418a05a
Kevin Swiber <kswiber@gmail.com>
2013-12-06 14:58:27 -0500

    Using Pipeworks for postBuild.

--- host/index.js
+++ host/index.js
@@ -1,4 +1,5 @@
 var argo = require('argo');
+var pipeworks = require('pipeworks');
 var router = require('argo-url-router');
 var BuildContainer = require('./build_container');
 var server = require('./config/server');
@@ -6,42 +7,68 @@ var server = require('./config/server');
 argo()
   .use(router)
   .post('/builds/{name}', function(handle) {
-    handle('request', function(env, next) {
-      var container = BuildContainer.create(server);
-
-      container.create(env, function(err, body) {
-        if (!body || err) {
-          env.response.statusCode = 500;
-          return next(env);
-        }
-
-        body = JSON.parse(body.toString());
-        var id = body.Id;
-
-        container.attach(env, id, function(err, client) {
-          if (err) {
-            env.response.statusCode = 500;
-            return next(env);
-          }
-
-          container.start(env, id, function(err) {
-            if (err) {
-              env.response.statusCode = 500;
-            }
-
-            client.end();
-
-            container.wait(env, id, function(err) {
-              if (err) {
-                console.log(err);
-                env.response.statusCode = 500;
-              }
-
-              next(env);
-            });
-          });
-        });
-      });
-    });
+    handle('request', postBuild);
   })
   .listen(process.env.PORT || 3000);
+
+var postBuild = function(env, next) {
+  var container = BuildContainer.create(server);
+
+  var pipeline = pipeworks();
+
+  pipeline.fit(function(context, next) {
+    container.create(context.env, function(err, body) {
+      if (!body || err) {
+        env.response.statusCode = 500;
+        return context.next(context.env);
+      }
+
+      body = JSON.parse(body.toString());
+      context.id = body.Id;
+
+      next(context);
+    });
+  });
+
+  pipeline.fit(function(context, next) {
+    container.attach(context.env, context.id, function(err, client) {
+      if (err) {
+        context.env.response.statusCode = 500;
+        return context.next(context.env);
+      }
+
+      context.client = client;
+      next(context);
+    });
+  });
+
+  pipeline.fit(function(context, next) {
+    container.start(context.env, context.id, function(err) {
+      if (err) {
+        env.response.statusCode = 500;
+        return context.next(context.env);
+      }
+
+      context.client.end();
+
+      next(context);
+    });
+  });
+
+  pipeline.fit(function(context, next) {
+    container.wait(context.env, context.id, function(err) {
+      if (err) {
+        console.log(err);
+        context.env.response.statusCode = 500;
+      }
+
+      next(context);
+    });
+  });
+
+  pipeline.fit(function(context, next) {
+    context.next(context.env);
+  });
+
+  pipeline.flow({ env: env, next: next });
+};
