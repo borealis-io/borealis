0f2b872cc37b2de6e473558aaa008cc27248507c
Kevin Swiber <kswiber@gmail.com>
2014-01-07 14:09:35 -0500

    Using environment variables for host and port.

--- host/index.js
+++ host/index.js
@@ -1,6 +1,9 @@
 var titan = require('titan');
 
+var port = process.env.PORT || 3000;
+var host = process.env.HOST;
+
 titan()
   .logger()
   .load()
-  .listen(process.env.PORT || 3000);
+  .listen(port, host);
